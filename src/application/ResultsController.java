dd903382a1923ecad7a8edaa070c85729b15a316
Abhinav Choudhary <choudhary.ab@northeastern.edu>
2022-11-23 19:55:06 -0500

    added switching between pages and dynamic path to images

--- src/application/ResultsController.java
+++ src/application/ResultsController.java
@@ -1,13 +1,46 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
-public class ResultsController {
+public class ResultsController implements Initializable {
 
 	@FXML ImageView resultImage;
 	@FXML Button saveButton;
 	@FXML Button doneButton;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		resultImage.setImage(new Image(Properties.getImagePath()));
+	}
+	
+	public void onDone(ActionEvent event) {
+		try {
+			System.out.printf("%nResults page --- ImagePath: %s%n Frequency: %.2f%n Material: %s", Properties.getImagePath(), Properties.getFrequency(), Properties.getMaterial());
+			
+			Stage newStage = (Stage) doneButton.getScene().getWindow();
+			Parent goBackRoot = FXMLLoader.load(getClass().getClassLoader().getResource("GoBack.fxml"));
+			Scene newScene = new Scene(goBackRoot);
+			newStage.setScene(newScene);
+			newStage.show();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void onSave(ActionEvent event) {
+		System.out.printf("%nSave Image.%n");
+	}
 
 }
